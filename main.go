59d932a1c5602cfa9b0c75b8e81b2707286c6808
hamster1963 <1410514192@qq.com>
2025-04-09 10:54:12 +0800

    feat: add CORS middleware to handle cross-origin requests

--- main.go
+++ main.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"tr-proxy/G2Proxy"
 )
 
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
+		// 如果是 OPTIONS 请求，直接返回状态码 204
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
+
+	// 使用跨域中间件
+	r.Use(Cors())
 
 	r.POST("/*path", func(c *gin.Context) {
 		G2Proxy.HandleToRPC(c)
